bb66977c43bc2f6bde12adc604ce238c11ebdc3d
Madhan Babu Uradi <umadhanbabu01@gmail.com>
2024-07-20 22:14:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import QuoteCard from './QuoteCard';
+import SavedQuotes from './SavedQuotes';
+import './App.css';
+
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [savedQuotes, setSavedQuotes] = useState([]);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+      setQuote(response.data[0]);
+    } catch (error) {
+      console.error('Error fetching quote', error);
+    }
+  };
+
+  const saveQuote = (quote) => {
+    setSavedQuotes((prevQuotes) => [...prevQuotes, quote]);
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>RANDOM QUOTES</h1>
+      <QuoteCard quote={quote} onSave={saveQuote} />
+      <button className="new-quote-button" onClick={fetchQuote}>New Quote</button>
+      <SavedQuotes quotes={savedQuotes} />
+    </div>
+  );
+};
+
+export default App;
